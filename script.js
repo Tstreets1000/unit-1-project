8b3741b8d7cd710b017c3383ec27610606820204
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-05-01 18:51:15 -0400

    Day 3 of my project and attendance

---
+++ script.js
@@ -0,0 +1,34 @@
+/*----- constants -----*/
+const screens = array.from()
+const b = blackPieces
+const w = whitePieces
+
+
+/*-----objects-------*/
+
+
+
+/*----- state variables -----*/
+
+
+
+/*----- cached elements  -----*/
+
+
+
+
+/*----- event listeners -----*/
+// When I click ENTER button, screen should roll over to Kingdom screen/ next screen.
+enterBtn.addEventListener('click', enter);
+// When I click START button, screen should roll over to Battlefield screen/ next screen.
+startBtn.addEventListener('click', start);
+
+ /*----- functions -----*/
+const enter = function() {
+  
+}
+
+const start = function() {
+
+}
+
