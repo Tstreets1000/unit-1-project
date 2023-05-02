60b7228185bddc11f0a4461ef413ddb87b2d119b
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-05-01 22:31:09 -0400

    Changes to buttons

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-/*----- constants -----*/
-const screens = array.from()
-const b = blackPieces
-const w = whitePieces
+
 
 
 /*-----objects-------*/
+
+
+
+
 
 
 
@@ -18,17 +19,30 @@ const w = whitePieces
 
 
 /*----- event listeners -----*/
-// When I click ENTER button, screen should roll over to Kingdom screen/ next screen.
-enterBtn.addEventListener('click', enter);
-// When I click START button, screen should roll over to Battlefield screen/ next screen.
-startBtn.addEventListener('click', start);
+// When I click the ENTER button, screen should roll over to Kingdom screen/ next screen.
+document.querySelector('.enterBtn').addEventListener('click', event => {
+  document.querySelector('.car1').style.display='none' 
+})
+
+// When I click the START button, screen should roll over to Battlefield screen/ next screen.
+document.querySelector('.startBtn').addEventListener('click', start => {
+  document.querySelector('.car2').style.display='none'
+})
+
+// When redArmy button is selected; stay highlighted, assign player 1 & default blue army player 2
+document.querySelector('.redArmyBtn').addEventListener('click', event => {
+  document.querySelector('.redArmyBtn').style.backgroundColor='#9F0000'
+  document.querySelector('.blueArmyBtn').style.backgroundColor='white'
+})
+// When blueArmy button is selected; stay highlighted, assign player 1 & default red army player 2
+document.querySelector('.blueArmyBtn').addEventListener('click', event => {
+  document.querySelector('.blueArmyBtn').style.backgroundColor='#020287'
+  document.querySelector('.redArmyBtn').style.backgroundColor='white'
+})
 
  /*----- functions -----*/
-const enter = function() {
-  
-}
 
-const start = function() {
+const start = function(event) {
 
 }
 
