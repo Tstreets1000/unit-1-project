d83134fa5c53702bb29885241d7bca0f44ba1286
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-05-01 23:05:51 -0400

    final commit for 05/01

--- script.js
+++ script.js
@@ -29,15 +29,20 @@ document.querySelector('.startBtn').addEventListener('click', start => {
   document.querySelector('.car2').style.display='none'
 })
 
+// When I click the PLAY AGAIN button, screen should roll back to Battlefield screen/ next screen.
+document.querySelector('.playAgainBtn').addEventListener('click', start => {
+  document.querySelector('.car3').style.display='.car2'
+})
+
 // When redArmy button is selected; stay highlighted, assign player 1 & default blue army player 2
 document.querySelector('.redArmyBtn').addEventListener('click', event => {
-  document.querySelector('.redArmyBtn').style.backgroundColor='#9F0000'
-  document.querySelector('.blueArmyBtn').style.backgroundColor='white'
+  document.querySelector('.redArmyBtn').style.backgroundColor='#9F0000' 
+  document.querySelector('.blueArmyBtn').style.backgroundColor='white' 
 })
 // When blueArmy button is selected; stay highlighted, assign player 1 & default red army player 2
 document.querySelector('.blueArmyBtn').addEventListener('click', event => {
-  document.querySelector('.blueArmyBtn').style.backgroundColor='#020287'
-  document.querySelector('.redArmyBtn').style.backgroundColor='white'
+  document.querySelector('.blueArmyBtn').style.backgroundColor='#020287' 
+  document.querySelector('.redArmyBtn').style.backgroundColor='white' 
 })
 
  /*----- functions -----*/
