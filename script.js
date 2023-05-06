2259b0bae45245fb65cdb6bec00beed451a8f8c5
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-05-05 22:57:22 -0400

    The struggle is real, but I'm tryin

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 /* ===== Constant Variables ===== */
-const squareSelect = [a1, a2, a3, a4, a5, a6, a7, a8, b1, b2, b3, b4, b5, b6, b7, b8]
+const BLUE_ARMY = {
+  'blue army': 'bPawnA', 
+  'blue army': 'bPawnB', 
+  'blue army': 'bPawnC', 
+  'blue army': 'bPawnD', 
+  'blue army': 'bPawnE', 
+  'blue army': 'bPawnF', 
+  'blue army': 'bPawnH', 
+  'blue army': 'bKing', 
+  'blue army': 'bQueen', 
+  'blue army': 'bBishop1', 
+  'blue army': 'bBishop2', 
+  'blue army': 'bKnight1', 
+  'blue army': 'bKnight2', 
+  'blue army': 'bRook1', 
+  'blue army': 'bRook2'
+}  
 
+const RED_ARMY = {
+  'red army': 'rPawnA', 
+  'red army': 'rPawnB', 
+  'red army': 'rPawnC', 
+  'red army': 'rPawnD', 
+  'red army': 'rPawnE', 
+  'red army': 'rPawnF', 
+  'red army': 'rPawnH', 
+  'red army': 'rKing', 
+  'red army': 'rQueen', 
+  'red army': 'rBishop1', 
+  'red army': 'rBishop2', 
+  'red army': 'rKnight1', 
+  'red army': 'rKnight2', 
+  'red army': 'rRook1', 
+  'red army': 'rRook2'
+}
 
-/*-----objects-------*/
-
-
-
-
-
-/*----- state variables -----*/
-
-
-
-
+class Player {
+  constructor() {
+    this.name = 'name'
+    this.armyColor = 'color'
+  }
+}
 
 /*----- cached elements  -----*/
-
-
+const enterBtn = document.querySelector('button')
+const startBtn = document.querySelector('button')
+const playAgainBtn = document.querySelector('button')
+const redArmyBtn = document.querySelector('button')
+const blueArmyBtn = document.querySelector('button')
 
 
 /* ===== (BUTTONS) EVENT LISTENERS ===== */
 // When I click the ENTER button, screen should roll over to Kingdom screen/ next screen.
-document.querySelector('.enterBtn').addEventListener('click', event => {
+document.querySelector('.enterBtn').addEventListener('click', enter => {
   document.querySelector('.car1').style.display='none' 
 })
 
@@ -31,51 +62,65 @@ document.querySelector('.startBtn').addEventListener('click', start => {
 })
 
 // When I click the PLAY AGAIN button, screen should roll back to Battlefield screen/ next screen.
-document.querySelector('.playAgainBtn').addEventListener('click', start => {
-  document.querySelector('.car3').style.display='.car2'
+document.querySelector('.playAgainBtn').addEventListener('click', init => {
 })
 
 // When redArmy button is selected; stay highlighted, assign player 1 & default blue army player 2
 document.querySelector('.redArmyBtn').addEventListener('click', event => {
   document.querySelector('.redArmyBtn').style.backgroundColor='#9F0000' 
-  document.querySelector('.blueArmyBtn').style.backgroundColor='white' 
+  document.querySelector('.blueArmyBtn').style.backgroundColor='white'
+  redArmyBtn.addEventListener('click', function() {
+  })
+  alert("Player 1: Red Army")
 })
+
 // When blueArmy button is selected; stay highlighted, assign player 1 & default red army player 2
 document.querySelector('.blueArmyBtn').addEventListener('click', event => {
   document.querySelector('.blueArmyBtn').style.backgroundColor='#020287' 
-  document.querySelector('.redArmyBtn').style.backgroundColor='white' 
+  document.querySelector('.redArmyBtn').style.backgroundColor='white'
+  blueArmyBtn.addEventListener('click', function() {
+  })
+  alert("Player 1: Blue Army") 
 })
 
+/* ===== FUNCTIONS ===== */
+function render() {
+  assignPlayer()
+  renderBoard()
+  renderMessage()
+  renderControls()
+}
+
+function init() {
+  renderBoard [
+    [rRook1, rKnight1, rBishop1, rQueen, rKing, rBishop2, rBishop2, rRook2]
+    [rPawnA, rPawnB, rPawnC, rPawnD, rPawnE, rPawnF, rPawnG, rPawnH]
+    [null, null, null, null, null, null, null, null]
+    [null, null, null, null, null, null, null, null]
+    [null, null, null, null, null, null, null, null]
+    [null, null, null, null, null, null, null, null]    
+    [bRook1, bKnight1, bBishop1, bQueen, bKing, bBishop2, bBishop2, bRook2]
+    [bPawnA, bPawnB, bPawnC, bPawnD, bPawnE, bPawnF, bPawnG, bPawnH]
+  ]
+}
+
 /* ===== (CHESS PIECES) EVENT LISTENERS ===== (example code found on techiedelight.com)===== */ 
-document.getElementById('bPawnA').draggable = function() {
-  const pieceMove = document.getElementById('bPawnA')
-  pieceMove.appendChild(document.getElementById('a3'))
-  document.getElementById('bPawnA').draggable('a3') 
+document.getElementById("bPawnA").draggable = function() {
+  const pieceMove = document.createDocumentFragment()
+  pieceMove.appendChild(document.getElementById("bPawnA"))
+  document.getElementById('a4').appendChild(pieceMove)   
 }
 
 
 /*----- (CHESS PIECES) Movement Statements -----*/
 
 // ROOKS - Can move lateral and horizontal across all 8 squares//
- //document.getElementById("rRook").innerHTML('click', event => {
-  
- //})
-
-
 // KNIGHTS - Can move in L-shape; total of 4 squares from starting point//
-
-
-
 // BISHOPS - Can move on diagonal across 8 squares//
-
-
-
 // QUEENS - Can move any direction; across all 8 squares//
-
-
-
 // KINGS - Can move any direction; one square at a time//
+// PAWNS - Can move forward to advance; Intial movement of 2 squares, then 1 after. Captures opposing piece on diagonal left/right./
 
 
 
-// PAWNS - Can move forward to advance; Intial movement of 2 squares, then 1 after. Captures opposing piece only diagonally./
+
